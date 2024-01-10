9573376f9632e92762f0d1f50b914ccc38d5cd67
Temayoo <Matt.yo95280@gmail.com>
2024-01-10 15:32:58 +0100

    ennemy

--- src/Controller/gamebase.java
+++ src/Controller/gamebase.java
@@ -1,5 +1,7 @@
 package Controller;
 
+
+import Model.Character.Ennemy;
 import Model.Character.Personnage;
 import Model.Character.Player;
 import Model.map;
@@ -8,20 +10,26 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import static View.cli.displayMap;
+import java.util.Random;
 
 public class gamebase {
 
 
     static map laMap = new map(22, 22);
-    public static Player e = new Player("test", 100, 1, "p",null);
+    public static Player e = new Player("test", 100, 1, "p",null, 0);
 
     public static void init()
     {
         laMap.generateMap();
 
         placePlayer(12, 12);
-        laMap.display();
         //displayMap();
+        spawnEnnemy(3);
+        placeEnnemy();
+
+        laMap.display();
+
+
     }
 
     public static void placePlayer(int x, int y)
@@ -29,6 +37,31 @@ public class gamebase {
         laMap.spawn(x, y, e.getSymbole());
         e.setCord(x, y);
     }
+
+    public static ArrayList<Ennemy> ennemies = new ArrayList<Ennemy>();
+
+    public static void spawnEnnemy(int nbrEnnemy){
+
+        for (int i = 0; i < nbrEnnemy ; i++) {
+            Ennemy mechant = new Ennemy("nomDeMechant",100,1,"X",null,false,false);
+            ennemies.add(mechant);
+        }
+    }
+
+    public static void placeEnnemy()
+    {
+        for (Ennemy mechant : ennemies) {
+
+            Random random = new Random();
+            int xRandom = random.nextInt(20);
+            int yRandom = random.nextInt(20);
+            laMap.spawn(xRandom,yRandom, mechant.getSymbole());
+            mechant.setCord(xRandom,yRandom);
+        }
+    }
+
+
+
 
 
 }
