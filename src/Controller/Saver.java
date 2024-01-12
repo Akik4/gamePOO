4e7492ec55b184416ba088b22c40b3ea0a9cd458
Lina <144618594+rinrina01@users.noreply.github.com>
2024-01-12 15:33:45 +0100

    Update Saver.java

--- src/Controller/Saver.java
+++ src/Controller/Saver.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 public class Saver {
 
     public static boolean CreateFile() throws IOException {
+        /** This function verifies if the save.txt
+         * file can be opened succesfully.
+         * @return boolean true if file opened successfully **/
         if(isFileNotExist())
         {
             File newFile = new File("save.txt");
@@ -20,11 +23,17 @@ public class Saver {
 
     protected static boolean isFileNotExist()
     {
+        /** This function verifies the save.txt
+         * file exists.
+         * @return boolean true if file found **/
         File newFile = new File("save.txt");
         return !newFile.exists();
     }
 
     public static Scanner readFile() throws FileNotFoundException {
+        /** This function reads the entire file and
+         * returns it.
+         @return (myFile) content of the save.txt file **/
         File newFile = new File("save.txt");
         Scanner myFile = new Scanner(newFile);
 //        while (myFile.hasNextLine())
@@ -40,6 +49,9 @@ public class Saver {
     }
 
     public static FileWriter getSaver() throws IOException {
+        /** This function creates a FileWriter object to
+         * access the file in write mode.
+         * @return FileWriter(newFile) FileWriter object **/
         File newFile = new File("save.txt");
         return new FileWriter(newFile);
     }
